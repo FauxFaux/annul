ffb7ec816cd023956eda62e567019d8b68672f7d
Chris West (Faux) <git@goeswhere.com>
2019-01-27 21:00:34 +0000

    skip binaries or something

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use std::env;
-use std::io::Write;
-use std::io;
 use std::fs;
+use std::io;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::io::Write;
 
-use byteorder::LE;
 use byteorder::WriteBytesExt;
+use byteorder::LE;
 use cast::u64;
+use failure::bail;
+use failure::ensure;
 use failure::err_msg;
 use failure::Error;
-use failure::bail;
 use failure::ResultExt;
+use iowrap::ReadMany;
 use splayers::Entry;
 use splayers::Status;
-use failure::ensure;
 
 fn main() -> Result<(), Error> {
     let src = env::args().nth(1).ok_or(err_msg("first arg: src"))?;
@@ -23,25 +26,28 @@ fn main() -> Result<(), Error> {
     let root = dest.parent().ok_or(err_msg("file output please"))?;
 
     let mut tmp = tempfile::NamedTempFile::new()?;
-    http_req::request::get(src, &mut tmp)
-        .with_context(|_| err_msg("downloading"))?;
+    http_req::request::get(src, &mut tmp).with_context(|_| err_msg("downloading"))?;
 
     let unpack = splayers::Unpack::unpack_into(tmp.path(), root)
         .with_context(|_| err_msg("unpacking failed"))?;
 
-    let mut out = tempfile_fast::PersistableTempFile::new_in(root)?;
+    let out = tempfile_fast::PersistableTempFile::new_in(root)?;
+
+    let mut out = zstd::Encoder::new(out, 0)?;
 
     match *unpack.status() {
         splayers::Status::Success(ref entries) => output(entries, &[], &mut out)?,
         ref other => bail!("expecting top level archive, not: {:?}", other),
     }
 
+    let out = out.finish()?;
+
     out.persist_noclobber(dest).map_err(|e| e.error)?;
 
     Ok(())
 }
 
-fn output(entries: &[Entry], paths: &[Box<[u8]>], mut out: &mut tempfile_fast::PersistableTempFile) -> Result<(), Error> {
+fn output<W: Write>(entries: &[Entry], paths: &[Box<[u8]>], out: &mut W) -> Result<(), Error> {
     let mut entries: Vec<&Entry> = entries.iter().collect();
 
     let mut name_prefix = Vec::with_capacity(paths.len() * 128);
@@ -53,38 +59,58 @@ fn output(entries: &[Entry], paths: &[Box<[u8]>], mut out: &mut tempfile_fast::P
     entries.sort_by_key(|e| e.local.path.as_ref());
 
     for entry in entries {
-        #[cfg(nah)]
-        let file = match entry.local.meta.item_type {
-            ItemType::RegularFile => true,
-            _ => false,
-        };
-
         let mut full_name = name_prefix.to_vec();
         full_name.extend_from_slice(&entry.local.path);
 
-        let data_len = if let Some(temp) = entry.local.temp.as_ref() {
-           temp.metadata()?.len()
-        } else {
-            0
-        };
+        // hmm, trying to make the name distinct from the content, for grepping
+        full_name.push(0);
 
         out.write_u64::<LE>(u64(full_name.len()))?;
-        out.write_u64::<LE>(data_len)?;
-        out.write_all(&full_name)?;
 
         if let Some(temp) = entry.local.temp.as_ref() {
-            let written = io::copy(&mut fs::File::open(temp)?, &mut out)?;
-            ensure!(written == data_len, "short write: expected: {}, actual: {}", data_len, written);
+            let mut file = fs::File::open(temp)?;
+            let mut header = [0u8; 8 * 1024];
+            let read = file.read_many(&mut header)?;
+
+            if likely_text(&header[..read]) {
+                let data_len = file.metadata()?.len();
+                out.write_u64::<LE>(data_len)?;
+                out.write_all(&full_name)?;
+
+                file.seek(SeekFrom::Start(0))?;
+
+                let written = io::copy(&mut file, out)?;
+                ensure!(
+                    written == data_len,
+                    "short write: expected: {}, actual: {}",
+                    data_len,
+                    written
+                );
+            } else {
+                out.write_u64::<LE>(0)?;
+                out.write_all(&full_name)?;
+            }
+        } else {
+            out.write_u64::<LE>(0)?;
+            out.write_all(&full_name)?;
         }
 
         match &entry.children {
             Status::Success(entries) => {
                 let mut paths = paths.to_vec();
                 paths.push(entry.local.path.clone());
-                output(&entries, &paths, &mut out)?;
+                output(&entries, &paths, out)?;
             }
             _ => (),
         }
     }
     Ok(())
+}
+
+fn likely_text(buf: &[u8]) -> bool {
+    if memchr::memchr(0, buf).is_some() {
+        return false;
+    }
+
+    !buf.iter().any(|&b| 4 == b || (b >= 5 && b <= 8) || (b >= 14 && b <= 26))
 }

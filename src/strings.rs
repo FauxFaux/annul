e668e1e5e910bb03b16ab491c018971de5c05349
Chris West (Faux) <git@goeswhere.com>
2019-02-05 17:03:58 +0000

    clearly that's an object

--- src/strings.rs
+++ src/strings.rs
@@ -1,4 +1,5 @@
-use std::io::Read;
+use std::io;
+use std::io::Write;
 
 struct UtfState {
     wanted: u8,
@@ -59,27 +60,6 @@ impl ShortArray {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-enum State {
-    Happy,
-    Suspicious(u8),
-    Miffed(u8, u8),
-    Angry,
-    Utf2(Vec<u8>),
-    Utf3(Vec<u8>),
-    Utf4(Vec<u8>),
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-enum Classification {
-    Binary,
-    Ascii,
-    Utf2,
-    Utf3,
-    Utf4,
-    UtfFollower,
-}
-
 fn get_char(bytes: &[u8]) -> Char {
     if bytes.is_empty() {
         return Char::Short(1);
@@ -123,122 +103,71 @@ fn follower(byte: u8) -> bool {
     byte & 0b1100_0000 == 0b1000_0000
 }
 
-fn classify(byte: u8) -> Classification {
-    if byte < b' ' && b'\t' != byte && b'\n' != byte && b'\r' != byte {
-        return Classification::Binary;
-    }
-
-    if byte < 0x7f {
-        return Classification::Ascii;
-    }
-
-    if byte & 0b1100_0000 == 0b1000_0000 {
-        return Classification::UtfFollower;
-    }
-
-    if byte & 0b1110_0000 == 0b1100_0000 {
-        return Classification::Utf2;
-    }
-
-    if byte & 0b1111_0000 == 0b1110_0000 {
-        return Classification::Utf3;
-    }
-
-    if byte & 0b1111_1000 == 0b1111_0000 {
-        return Classification::Utf4;
-    }
-
-    Classification::Binary
-}
-
 fn find_chars(data: &[u8]) -> Vec<Char> {
     let mut chars = Vec::with_capacity(data.len());
 
     let mut ptr = data;
     while !ptr.is_empty() {
         let c = get_char(ptr);
-        match c {
-            Char::Short(missing) => {
-                // TODO
-                assert_eq!(0, missing);
-                break;
-            },
-            other => chars.push(other),
-        }
+        chars.push(c);
         ptr = &ptr[c.len()..];
     }
 
     chars
 }
 
-fn strings(data: &[u8]) -> Vec<u8> {
-    let mut out = Vec::with_capacity(data.len());
-    let mut buf = Vec::with_capacity(12);
-    let mut binaries = 0;
+#[derive(Clone, Debug)]
+struct StringState {
+    buf: Vec<u8>,
+    binaries: usize,
+}
+
+fn strings<W: Write>(data: &[u8], state: &mut StringState, mut out: W) -> Result<(), io::Error> {
     for c in find_chars(data) {
         match c {
-            Char::Binary(c) if binaries < 2 => {
-                binaries += 1;
-                buf.push(c);
+            Char::Binary(c) if state.binaries < 2 => {
+                state.binaries += 1;
+                state.buf.push(c);
             }
 
             Char::Binary(_) => {
-                for _ in 0..binaries {
-                    assert!(buf.pop().is_some());
+                for _ in 0..state.binaries {
+                    assert!(state.buf.pop().is_some());
                 }
 
-                if buf.len() > 3 {
-                    out.extend_from_slice(&buf);
-                    out.push(0);
+                if state.buf.len() > 3 {
+                    out.write_all(&state.buf)?;
+                    out.write_all(&[0])?;
                 }
-                binaries = 0;
-                buf.clear()
+                state.binaries = 0;
+                state.buf.clear()
             },
             Char::Printable(arr) => {
-                if binaries == buf.len() {
-                    buf.clear();
+                if state.binaries == state.buf.len() {
+                    state.buf.clear();
                 }
-                arr.push_to(&mut buf);
-                binaries = 0;
+                arr.push_to(&mut state.buf);
+                state.binaries = 0;
             },
             Char::Short(_) => unimplemented!(),
         }
     }
-    out.extend_from_slice(&buf);
-    out
-}
-
-#[cfg(never)]
-fn satrings(data: &[u8]) -> Vec<u8> {
-    let mut ret = Vec::with_capacity(data.len());
-
-    let mut state = State::Happy;
-
-    for &b in data {
-        let classification = classify(b);
-        state = match state {
-            State::Happy => match classification {
-                Classification::Ascii => {
-                    ret.push(b);
-                    State::Happy
-                }
-                Classification::Binary | Classification::UtfFollower => State::Suspicious(b),
-                Classification::Utf2 => State::Utf2(vec![b]),
-                Classification::Utf3 => State::Utf3(vec![b]),
-                Classification::Utf4 => State::Utf4(vec![b]),
-            },
-        }
-    }
-
-    ret
+    out.write_all(&state.buf)?;
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::strings;
+    use super::StringState;
 
     fn check(expected: &[u8], data: &[u8]) {
-        let actual = strings(data);
+        let mut state = StringState {
+            buf: Vec::with_capacity(128),
+            binaries: 0,
+        };
+        let mut actual = Vec::new();
+        strings(data, &mut state, &mut actual).expect("only for vec");
 
         assert_eq!(
             String::from_utf8_lossy(expected),
